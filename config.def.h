a46c4e1ae661074128b7ca4e2fa17956027b3c5b
Jessy Provencher <jessyprovencher@tuta.io>
2021-05-14 21:32:15 -0400

    Fix 'teams' not working properly

--- config.def.h
+++ config.def.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "teams",      NULL,     NULL,        0,         1,          0,          -1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
